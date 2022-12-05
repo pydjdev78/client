c3f58000f95169c2dac6933d5d726e2333e65d36
pydjdev78 <63595980+pydjdev78@users.noreply.github.com>
2022-12-05 09:26:51 +0600

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 
-
+import rook
 def ton():
     a = 4
     b = 6
     print(a + b)
 
 if __name__ == '__main__':
+    rook.start(token='499178836c84e60592d7c1f33866fa93e957b191fe5b8169192403760c6cae65', labels={"env":"dev"})
     ton()
